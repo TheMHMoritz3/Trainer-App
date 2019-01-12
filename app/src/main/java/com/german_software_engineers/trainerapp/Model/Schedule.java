3aa1868429b36f2c1a9bcbff781c28c8c0c63671
Moritz Herzog <mhmoritz3@outlook.com>
2019-01-12 21:29:28 +0100

    Continuing Model Work

--- app/src/main/java/com/german_software_engineers/trainerapp/Model/Schedule.java
+++ app/src/main/java/com/german_software_engineers/trainerapp/Model/Schedule.java
@@ -1,4 +1,28 @@
 package com.german_software_engineers.trainerapp.Model;
 
+
 public class Schedule {
+    private String Name;
+    private TrainingsTypes TrainingsType;
+    private Intensities WarmUpIntensity;
+    private int Repeations;
+    private int PauseTime;
+    private int Sets;
+    private int Speed;
+    private String WarmUpExcersize;
+    private int WarmUpTime;
+    private int BPM;
+
+    public Schedule(String name, TrainingsTypes trainingsType, int repeations, int pauseTime, int sets, int speed, String warmUpEx, int warmUpTime, Intensities intensity, int bpm){
+        Name = name;
+        WarmUpIntensity = intensity;
+        TrainingsType = trainingsType;
+        Repeations = repeations;
+        PauseTime = pauseTime;
+        Sets = sets;
+        Speed = speed;
+        WarmUpExcersize = warmUpEx;
+        WarmUpTime = warmUpTime;
+        BPM = bpm;
+    }
 }
---
+++ app/src/main/java/com/german_software_engineers/trainerapp/Model/TrainingsTypes.java
@@ -0,0 +1,4 @@
+package com.german_software_engineers.trainerapp.Model;
+
+public enum TrainingsTypes {
+}
