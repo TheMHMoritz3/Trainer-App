f5bf35604c32b98b6b6a66787f98db940dd60cea
Moritz Herzog <mhmoritz3@outlook.com>
2019-03-17 16:33:25 +0100

    Adding Some UnitTests not Filled Currently

---
+++ TrainingsModel/src/test/java/Exercise/ExerciseTest.java
@@ -0,0 +1,39 @@
+package Exercise;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+public class ExerciseTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+
+    @Test
+    public void equals() {
+    }
+
+    @Test
+    public void getPosition() {
+    }
+
+    @Test
+    public void setPosition() {
+    }
+}
