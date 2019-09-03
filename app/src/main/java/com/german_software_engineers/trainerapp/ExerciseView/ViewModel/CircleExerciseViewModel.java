3b8bb981132577a5f37f20c3f19bac9c852f3990
Moritz Herzog <moritz.herzog@german-software-engineers.com>
2019-09-03 18:07:31 +0200

    Fixing CI

--- app/src/main/java/com/german_software_engineers/trainerapp/ExerciseView/ViewModel/CircleExerciseViewModel.java
+++ app/src/main/java/com/german_software_engineers/trainerapp/ExerciseView/ViewModel/CircleExerciseViewModel.java
@@ -2,11 +2,15 @@ package com.german_software_engineers.trainerapp.ExerciseView.ViewModel;
 
 import android.arch.lifecycle.ViewModel;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 import Exercise.Exercise;
 
 public class CircleExerciseViewModel extends ViewModel {
 
-    public List<Exercise> getExercises()
+    public List<Exercise> getExercises(){
+        return new ArrayList<Exercise>();
+    }
 }
